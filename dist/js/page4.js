05fc43e4e0cc148363c67716af1da9d6c0cccf43
Egor <egorbobin74@gmail.com>
2024-10-28 17:08:24 +0500

    done

--- dist/js/page4.js
+++ dist/js/page4.js
@@ -3,43 +3,22 @@ const swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
   spaceBetween: 0,
   loop: false,
+  on: {
+    slideChange: function () {
+      const footer = document.querySelector(".footer");
+      if (swiper.activeIndex === 1) {
+        footer.style.display = "none";
+      } else {
+        footer.style.display = "block";
+      }
+    },
+  },
 });
-
-
-
-const videoPlayer = videojs("fullscreen-video");
-const icon1 = document.getElementById("icon1");
-
-swiper.on("slideChange", function () {
-  if (!videoPlayer.paused()) {
-    videoPlayer.pause();
-  }
-});
-
 
 document.addEventListener("keydown", function (event) {
-
   if (event.code === "ArrowRight") {
-    swiper.slideNext(); 
+    swiper.slideNext();
   } else if (event.code === "ArrowLeft") {
-    swiper.slidePrev(); 
+    swiper.slidePrev();
   }
-});
-
-
-videoPlayer.on("play", function () {
-  swiper.allowTouchMove = false; 
-  icon1.style.display = "none";
-});
-
-
-videoPlayer.on("pause", function () {
-  swiper.allowTouchMove = true; 
-  icon1.style.display = "block";
-});
-
-
-videoPlayer.on("ended", function () {
-  swiper.allowTouchMove = true;
-  icon1.style.display = "block";
 });
