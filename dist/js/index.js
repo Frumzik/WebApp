6e4e4ab22f81e853c02474db8fdf65623388b096
Egor <egorbobin74@gmail.com>
2024-11-24 19:56:32 +0500

    lastfixx4

--- dist/js/index.js
+++ dist/js/index.js
@@ -7,16 +7,12 @@ const amountInput = document.getElementById("amount");
 
 const swiper = new Swiper('.swiper-page1', {
     slidesPerView: 4,
-    spaceBetween: 10, 
+    spaceBetween: 10,
     freeMode: true,
-    breakpoints:{
-        768:{
-            slidesPerView: 5,
-        },
-        992:{
-            slidesPerView: 6,
-        },
-    }
+    breakpoints: {
+        768: { slidesPerView: 5 },
+        992: { slidesPerView: 6 },
+    },
 });
 
 amountInput.addEventListener("input", function (event) {
@@ -51,10 +47,9 @@ function LoadApiData() {
     const currentPath = window.location.pathname;
     const initData = btoa(window.Telegram.WebApp.initData);
     const params = new URLSearchParams({
-    lang: i18next.language,
+        lang: i18next.language,
     }).toString();
 
-    let notices = [];
     fetch(`/api/user/profile/?${params}`, {
         headers: { 'X-Telegram-Init-Data': initData },
         method: "GET",
@@ -62,18 +57,18 @@ function LoadApiData() {
     .then((response) => redirectNotAuthorized(response))
     .then((data) => {
         const firstName = data.user.firstName.length > 10 ? data.user.firstName.slice(0, 10) : data.user.firstName;
-        let lastName;
-        if (data.user.lastName === null) lastName = '';
-        else lastName = data.user.lastName.length > 10 ? data.user.lastName.slice(0, 10) : data.user.lastName;
+        const lastName = data.user.lastName 
+            ? (data.user.lastName.length > 10 ? data.user.lastName.slice(0, 10) : data.user.lastName)
+            : '';
         const userName = `${firstName} ${lastName}`;
         const balance = data.user.balance;
         const name = data.series.name;
         const number = data.series.number;
         const progressValue = data.progress;
         const newsArray = data.news;
+
         notices = data.notices;
         displayNotifications(notices);
-
 
         document.getElementById("userName").innerHTML = userName;
         document.getElementById("balance").textContent = balance;
@@ -85,18 +80,22 @@ function LoadApiData() {
         document.getElementById("iconLink").href = data.iconLink;
         document.getElementById("supportLink").href = data.supportLink;
 
-        for (let i = 0; i < newsArray.length; i++) {
+        const newsContainer = document.getElementById("newsContainer");
+        newsContainer.innerHTML = "";
+
+        for (let i = 0; i < newsArray.length; i++) { 
             let newsElement = `<div class="swiper-slide my-slide">
                                 <a href="${newsArray[i].telegraphLink}" class="advertising__blocks">
                                     <img src='${newsArray[i].imageLink}' style='height: inherit; border-radius: 10px; width: 100%;'>
                                 </a>
                             </div>`;
-            document.getElementById("newsContainer").innerHTML += newsElement;
+            newsContainer.insertAdjacentHTML('beforeend', newsElement);
         }
+
+        swiper.update();
     })
     .catch((error) => console.error("Ошибка:", error));
 }
-
 document.addEventListener("DOMContentLoaded", initLanguages(LoadApiData));
 i18next.on('languageChanged', LoadApiData);
 
